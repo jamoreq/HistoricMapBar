e88206edec984f837903d0d5bfcd297bba43321c
Jamrozowicz_Mateusz <jamoreq@gmail.com>
2022-05-13 16:16:09 +0200

    Drobne zmiany i poprawka błędów

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -25,6 +25,7 @@ class CreateNewUser implements CreatesNewUsers
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
+			'g-recaptcha-response' => 'required|captcha',
         ])->validate();
 
         return User::create([
