8e6d2a33aba3de5c39f7c1b8a805eb279d9be4db
Jamrozowicz_Mateusz <jamoreq@gmail.com>
2022-05-17 12:13:09 +0200

    Dodanie opisu artykułu i drobne poprawki wyglądu

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,7 +8,7 @@
     <div>
         <div class="max-w-6xl mx-auto py-10 sm:px-6 lg:px-8">
             <div class="block mb-8">
-                <a href="{{ route('tasks.create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Add Task</a>
+                <a href="{{ route('tasks.create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Dodaj artykuł</a>
             </div>
             <div class="flex flex-col">
                 <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -23,9 +23,16 @@
                                     <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                         Nazwa artykułu
                                     </th>
+									
+									<th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Opis artykułu
+                                    </th>
+									
                                     <th scope="col" width="200" class="px-6 py-3 bg-gray-50">
 
                                     </th>
+									
+									
                                 </tr>
                                 </thead>
                                 <tbody class="bg-white divide-y divide-gray-200">
@@ -36,10 +43,16 @@
                                         </td>
 
                                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                            {{ $task->name }}
+                                        </td>
+										
+										<td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
                                             {{ $task->description }}
                                         </td>
 
                                         <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+										
+
                                             <a href="{{ route('tasks.show', $task->id) }}" class="text-blue-600 hover:text-blue-900 mb-2 mr-2">Przeglądaj</a>
                                             <a href="{{ route('tasks.edit', $task->id) }}" class="text-indigo-600 hover:text-indigo-900 mb-2 mr-2">Edytuj</a>
                                             <form class="inline-block" action="{{ route('tasks.destroy', $task->id) }}" method="POST" onsubmit="return confirm('Na pewno chcesz usunąć ten artykuł?');">
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,7 +8,7 @@
     <div>
         <div class="max-w-6xl mx-auto py-10 sm:px-6 lg:px-8">
             <div class="block mb-8">
-                <a href="{{ route('users.create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Add User</a>
+                <a href="{{ route('users.create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Dodaj użytkownika</a>
             </div>
             <div class="flex flex-col">
                 <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
